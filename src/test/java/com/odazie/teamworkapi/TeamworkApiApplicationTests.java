a77c0688a3108b391b81db85e9e91c7e3ccf4937
Kikiodazie <kiki.odazie@yahoo.com>
2020-11-24 13:54:05 +0100

    Initial Commit: Setup

---
+++ src/test/java/com/odazie/teamworkapi/TeamworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odazie.teamworkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamworkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
