bff0b40879d20c944c137bdf13b80c94eba3cfcc
Kikiodazie <kiki.odazie@yahoo.com>
2020-12-12 02:44:30 +0100

    Feat: Admin/Employee can signin & modify create-user endpoint access for admin only

--- src/main/java/com/odazie/teamworkapi/webRestControllers/UserRestController.java
+++ src/main/java/com/odazie/teamworkapi/webRestControllers/UserRestController.java
@@ -1,5 +1,6 @@
 package com.odazie.teamworkapi.webRestControllers;
 
+import com.odazie.teamworkapi.business.model.AuthToken;
 import com.odazie.teamworkapi.business.service.UserService;
 import com.odazie.teamworkapi.data.entity.User;
 import com.odazie.teamworkapi.data.repository.JobRolesRepository;
@@ -7,7 +8,12 @@ import com.odazie.teamworkapi.securityConfig.TokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,6 +40,7 @@ public class UserRestController {
 
 
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/auth/create-user")
     public ResponseEntity<Void> userSignUp(@RequestBody User user){
         if (userService.getUserRepository().findUserByEmail(user.getEmail()) != null){
@@ -43,5 +50,21 @@ public class UserRestController {
         userService.saveUser(user);
         return new ResponseEntity<>( HttpStatus.CREATED);
     }
+
+    @PostMapping("/auth/signin")
+    public ResponseEntity<?> loginUser(@RequestBody User currentUser) throws AuthenticationException {
+
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        currentUser.getEmail(),
+                        currentUser.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        final String token = jwtTokenUtil.generateToken(authentication);
+        return ResponseEntity.ok(new AuthToken(token));
+    }
+
+
 
 }
