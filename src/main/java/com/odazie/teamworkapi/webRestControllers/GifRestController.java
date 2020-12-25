81f21d36fbf2ebcb76907cefd15aa0b323aa7f3a
Kikiodazie <kiki.odazie@yahoo.com>
2020-12-25 22:49:55 +0100

    Feat: Post gif functionality, save gif cloudinary url to DB

--- src/main/java/com/odazie/teamworkapi/webRestControllers/GifRestController.java
+++ src/main/java/com/odazie/teamworkapi/webRestControllers/GifRestController.java
@@ -3,10 +3,14 @@ package com.odazie.teamworkapi.webRestControllers;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import com.odazie.teamworkapi.business.service.CloudinaryService;
+import com.odazie.teamworkapi.business.service.UserService;
+import com.odazie.teamworkapi.data.entity.Gif;
+import com.odazie.teamworkapi.data.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,16 +24,26 @@ import java.util.Map;
 @RestController
 public class GifRestController {
 
-    @Autowired
-    private CloudinaryService cloudinaryService;
 
-    Cloudinary cloudinary;
+    private final CloudinaryService cloudinaryService;
+    private final UserService userService;
 
-    @PostMapping("/upload")
-    public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
-        String url = cloudinaryService.uploadFile(file);
-        return "File uploaded YOU DID IT successfully: File path :  " + url;
+    public GifRestController(CloudinaryService cloudinaryService, UserService userService) {
+        this.cloudinaryService = cloudinaryService;
+        this.userService = userService;
     }
+
+
+    @PreAuthorize("hasRole('EMPLOYEE')")
+    @PostMapping("/gifs")
+    public ResponseEntity<Void> uploadGif(@RequestParam("gifFile") MultipartFile gifFile, Authentication authentication, @RequestParam("title") String title) throws IOException {
+        User currentUser = userService.findUserByEmail(authentication.getName());
+        String url = cloudinaryService.uploadFile(gifFile);
+        cloudinaryService.saveGifToDB(url, title , currentUser);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+
 
 
 
